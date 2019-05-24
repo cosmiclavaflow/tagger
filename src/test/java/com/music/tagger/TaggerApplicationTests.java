6e07b1c6e3574f069ffe1bd6889c7fccfb69eed1
Artem Nuzhnyi <cosmiclavaflow@gmail.com>
2019-05-24 22:55:39 +0300

    Initial commit

---
+++ src/test/java/com/music/tagger/TaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.tagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
